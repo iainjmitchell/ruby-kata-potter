d172eb71c0272ebbd3b994638cfa1ce07e30d3a8
iainjmitchell <iainjmitchell@gmail.com>
2012-09-04 20:52:34 +0100

    all simple discounts applied

--- potter-tests.rb
+++ potter-tests.rb
@@ -5,44 +5,52 @@ class TestPotterPricer < Test::Unit::TestCase
 		8
 	end
 
+	def get_book_discounts
+		BookDiscounts.new()
+			.add(2, 0.95)
+			.add(3, 0.90)
+			.add(4, 0.80)
+			.add(5, 0.75)
+	end
+
 	def test_0_book_total_equals_0
 		basket = Basket.new()
-		target = PotterPricer.new(bookPrice, 0.95)
+		target = PotterPricer.new(bookPrice, get_book_discounts)
 		assert_equal 0, target.price(basket)
 	end
 
 	def test_1_book_one_total_equals_8
 		basket = Basket.new()
 			.add_book(1)
-		target = PotterPricer.new(bookPrice, 0.95)
+		target = PotterPricer.new(bookPrice, get_book_discounts)
 		assert_equal bookPrice, target.price(basket)
 	end
 
 	def test_1_book_two_total_equals_8
 		basket = Basket.new()
 			.add_book(2)
-		target = PotterPricer.new(bookPrice, 0.95)
+		target = PotterPricer.new(bookPrice, get_book_discounts)
 		assert_equal bookPrice, target.price(basket)
 	end
 
 	def test_1_book_three_total_equals_8
 		basket = Basket.new()
 			.add_book(3)
-		target = PotterPricer.new(bookPrice, 0.95)
+		target = PotterPricer.new(bookPrice, get_book_discounts)
 		assert_equal bookPrice, target.price(basket)
 	end
 
 	def test_1_book_four_total_equals_8
 		basket = Basket.new()
 			.add_book(4)
-		target = PotterPricer.new(bookPrice, 0.95)
+		target = PotterPricer.new(bookPrice, get_book_discounts)
 		assert_equal bookPrice, target.price(basket)
 	end
 
 	def test_1_book_five_total_equals_8
 		basket = Basket.new()
 			.add_book(5)
-		target = PotterPricer.new(bookPrice, 0.95)
+		target = PotterPricer.new(bookPrice, get_book_discounts)
 		assert_equal bookPrice, target.price(basket)
 	end
 
@@ -51,44 +59,76 @@ class TestPotterPricer < Test::Unit::TestCase
 		basket = Basket.new()
 			.add_book(bookId)
 			.add_book(bookId)
-		target = PotterPricer.new(bookPrice, 0.95)
+		target = PotterPricer.new(bookPrice, get_book_discounts)
 		assert_equal bookPrice * 2, target.price(basket)
 	end
 
 	def test_2_different_books_total_equals_twice_price_minus_5_percent
-		discount_percentage = 0.95
+		book_discounts = get_book_discounts()
 		basket = Basket.new()
 			.add_book(1)
 			.add_book(2)
-		target = PotterPricer.new(bookPrice, discount_percentage)
-		assert_equal (bookPrice * 2) * discount_percentage, target.price(basket)
+		target = PotterPricer.new(bookPrice, book_discounts)
+		assert_equal (bookPrice * 2) * book_discounts.get_discount_percentage_for(2), target.price(basket)
 	end
 
-	def test_another_2_different_books_total_equals_twice_price_minus_5_percent
-		discount_percentage = 0.95
+	def test_3_different_books_total_equals_three_times_price_minus_10_percent
+		book_discounts = get_book_discounts()
 		basket = Basket.new()
+			.add_book(1)
+			.add_book(2)
+			.add_book(3)
+		target = PotterPricer.new(bookPrice, book_discounts)
+		assert_equal (bookPrice * 3) * book_discounts.get_discount_percentage_for(3), target.price(basket)
+	end
+
+	def test_4_different_books_total_equals_four_times_price_minus_10_percent
+		book_discounts = get_book_discounts()
+		basket = Basket.new()
+			.add_book(1)
+			.add_book(2)
 			.add_book(3)
 			.add_book(4)
-		target = PotterPricer.new(bookPrice, discount_percentage)
-		assert_equal (bookPrice * 2) * discount_percentage, target.price(basket)
+		target = PotterPricer.new(bookPrice, book_discounts)
+		assert_equal (bookPrice * 4) * book_discounts.get_discount_percentage_for(4), target.price(basket)
+	end
+
+	def test_5_different_books_total_equals_four_times_price_minus_10_percent
+		book_discounts = get_book_discounts()
+		basket = Basket.new()
+			.add_book(1)
+			.add_book(2)
+			.add_book(3)
+			.add_book(4)
+			.add_book(5)
+		target = PotterPricer.new(bookPrice, book_discounts)
+		assert_equal (bookPrice * 5) * book_discounts.get_discount_percentage_for(5), target.price(basket)
 	end
 end
 
 class PotterPricer
-	def initialize(price, discount_percent)
+	def initialize(price, book_discounts)
 		@price = price
-		@discount_percent = discount_percent
+		@book_discounts = book_discounts
 	end
 
 	def price(basket)
-		return basket.books.length * @price  * @discount_percent  if contains_discount?(basket)
-		return basket.books.length * @price
+		return basket.books.length * @price  * @book_discounts.get_discount_percentage_for(basket.books.uniq.length) 
+	end
+end
+
+class BookDiscounts
+	def initialize()
+		@discount_amounts = Hash.new()
 	end
 
-	private 
+	def add(number_of_unique_books, discount_amount)
+		@discount_amounts[number_of_unique_books] = discount_amount
+		self
+	end
 
-	def contains_discount?(basket)
-		basket.books.uniq.length == 2
+	def get_discount_percentage_for(number_of_unique_books)
+		@discount_amounts.include?(number_of_unique_books) ? @discount_amounts[number_of_unique_books] : 1
 	end
 end
 
